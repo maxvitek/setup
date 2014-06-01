a881576c9ec7224310627c7bb5c379e6e69c7b15
maxvitek <maxvitek@gmail.com>
2014-06-01 08:22:47 -0500

    fixing setup

--- setup.sh
+++ setup.sh
@@ -4,10 +4,8 @@ rm ~/.bash_profile
 rm ~/.grcat
 rm ~/.my.cnf
 rm ~/.vimrc
-rm -r ~/.vim
 
 ln -s ${DIR}/.bash_profile ~/.bash_profile
 ln -s ${DIR}/.grcat ~/.grcat
 ln -s ${DIR}/.my.cnf ~/.my.cnf
 ln -s ${DIR}/.vimrc ~/.vimrc
-ln -s ${DIR}/.vim ~/.vim
